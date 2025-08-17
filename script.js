645a11981401377bc33bc116e570fb8395bd19c3
nh <nickxhan@umich.edu>
2025-08-17 03:12:04 -0400

    About to check for all possible win conditions
    Original idea was to only check around the most
    recently clicked cell, but that probably ends up
    being more inefficient. At the very least, it is
    far more difficult.

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ const board = (function () {
         }
     }
 
+    //1D is easier to check conditions for
+    const convertTo1D = function(row, column) {
+        return row * 3 + column;
+    }
+
+    const check2DWith1D = function(index) {
+        const row = 
+        return board
+    }
     //this just checks for a victory; the game controller will handle the actual victory logic
     const checkVictory = function(row, column, playerValue) {
         //only need to check for the just-changed part
-        //diagonal
-        if ((row === 2 && column == 2)) {
-            
+        const index = convertTo1D(row, column);
+        //if on one of the four corners and the center matches
+        if (index != 4 && index % 2 == 0 && board[1][1] === playerValue) {
+            if (Math.abs(index - 8))
         }
     }
 
